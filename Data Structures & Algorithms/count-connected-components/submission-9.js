c5c50330dcfaf7db17172e7be161a814b326cca2
Gulam Mohammad Ali <88267622+GMohammadAli@users.noreply.github.com>
2026-05-08 01:14:28 +0530

    Add: count-connected-components - submission-9

---
+++ Data Structures & Algorithms/count-connected-components/submission-9.js
@@ -0,0 +1,55 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {number}
+     */
+    countComponents(n, edges) {
+        const graph = [];
+
+        for(let i = 0; i < n; i++) {
+            graph.push([]);
+        }
+
+        for(let i = 0; i < edges.length; i++) {
+            let edge = edges[i];
+            graph[edge[0]].push(edge[1]);
+            graph[edge[1]].push(edge[0]);
+        }
+
+        const visited = new Set();
+        let numOfConnectedComponents = 0;
+
+        const dfs = (vertex) => {
+            if(visited.has(vertex)) return;
+
+            visited.add(vertex);
+            let n = graph[vertex].length;
+            for(let i = 0; i < n; i++) {
+                let neighbor = graph[vertex][i];
+                dfs(neighbor);
+            }
+        }
+
+        const bfsQueue = [];
+
+        for(let i = 0; i < n; i++) {
+            if(visited.has(i)) continue;
+            numOfConnectedComponents++;
+            bfsQueue.push(i);
+
+            while(bfsQueue.length) {
+                const vertex = bfsQueue.shift();
+
+                visited.add(vertex);
+                for(let i = 0; i < graph[vertex].length; i++) {
+                    const neighbor = graph[vertex][i];
+                    if(visited.has(neighbor)) continue;
+                    bfsQueue.push(neighbor);
+                }
+            }
+        }
+
+        return numOfConnectedComponents;
+    }
+}
