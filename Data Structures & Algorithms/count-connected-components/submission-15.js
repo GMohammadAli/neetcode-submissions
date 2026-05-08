6e6e53c91560c5dab07d338a2a341f4ef88e0cd0
Gulam Mohammad Ali <88267622+GMohammadAli@users.noreply.github.com>
2026-05-08 22:36:23 +0530

    Add: count-connected-components - submission-15

---
+++ Data Structures & Algorithms/count-connected-components/submission-15.js
@@ -0,0 +1,54 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {number}
+     */
+    countComponents(n, edges) {
+        const parent = [];
+        const rank = [];
+
+        for(let i = 0; i < n; i++) {
+            parent.push(i);
+            rank.push(1);
+        }
+
+        const findParent = (vertex) => {
+            let curr = vertex;
+
+            while(curr != parent[curr]) {
+                parent[curr] = parent[parent[curr]]; //path compression
+                curr = parent[curr];//replacing and finding parent until it is found
+            }
+
+            return curr;
+        }
+
+        const union = (u, v) => {
+            const pU = findParent(u);
+            const pV = findParent(v);
+
+            if(pU == pV) return false;
+            if(rank[pU] > rank[pV]) {
+                parent[pV] = pU;
+                rank[pU] += rank[pV];
+            } else {
+                parent[pU] = pV;
+                rank[pV] += rank[pU];
+            }
+
+            return true;
+        }
+
+        
+        let numOfConnected = n;
+        for(let i = 0; i < edges.length; i++) {
+            const edge = edges[i];
+            if(union(edge[0], edge[1])) {
+                numOfConnected--;
+            }
+        }
+
+        return numOfConnected;
+    }
+}
