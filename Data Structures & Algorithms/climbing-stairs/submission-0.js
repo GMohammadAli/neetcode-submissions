2271ce50002ebd2c3376e1addf396003af282957
Gulam Mohammad Ali <88267622+GMohammadAli@users.noreply.github.com>
2026-05-30 00:22:54 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n) {
+        const memo = [];
+        for(let i = 0; i <= n; i++) {
+            memo.push(-1);
+        }
+
+        const climbStairs = (n) => {
+            if(n == 0) return 1;
+            if(n < 0) return 0;
+
+            if(memo[n] != -1) return memo[n];
+
+            return memo[n] = climbStairs(n - 1) + climbStairs(n - 2);
+        }
+
+        return climbStairs(n);
+    }
+}
